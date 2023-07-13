ddb64bfca77f1e589a2a872ca06764a850f2e7a0
Ronsher14 <71829386+Ronsher14@users.noreply.github.com>
2023-07-13 16:09:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/assignment3_20bce0998/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.assignment3_20bce0998
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.assignment3_20bce0998.ui.theme.Assignment3_20BCE0998Theme
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Assignment3_20BCE0998Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    YoutubeScreen(videoId =
+                    "hEq34Cgb5PU",modifier=Modifier.fillMaxWidth())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun YoutubeScreen(
+    videoId: String,
+    modifier: Modifier
+) {
+    val ctx = LocalContext.current
+    AndroidView(factory = {
+        var view = YouTubePlayerView(it)
+        val fragment = view.addYouTubePlayerListener(
+            object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    super.onReady(youTubePlayer)
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+            }
+        )
+        view
+    })
+}
